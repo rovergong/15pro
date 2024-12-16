1f75178426ccbecc4c3c7b4d930cdaf522b66e07
rovergong <317907660@qq.com>
2024-12-16 18:07:56 +0800

    test2

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Configure base.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
@@ -22,6 +25,30 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 PRODUCT_PACKAGES += \
     bootctrl.xiaomi_sm8750.recovery \
     android.hardware.boot@1.2-impl-qti.recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+# API levels
+BOARD_API_LEVEL := 202404
+PRODUCT_SHIPPING_API_LEVEL := 35
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 32
