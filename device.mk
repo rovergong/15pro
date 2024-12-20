7766324b91e9e7c970a8a9b9a042efe76d8f80f7
rovergong <317907660@qq.com>
2024-12-20 19:55:58 +0800

    test_a1

--- device.mk
+++ device.mk
@@ -20,14 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_PACKAGES += \
-    bootctrl.xiaomi_sm8750.recovery \
+    bootctrl.xiaomi_sm8550.recovery \
     android.hardware.boot@1.2-impl-qti.recovery
+    android.hardware.health@2.0-impl-default.recovery
+    android.hardware.health@2.0-impl-2.1.recovery
 
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 32
 
 # VNDK API
-PRODUCT_TARGET_VNDK_VERSION := 32
+PRODUCT_TARGET_VNDK_VERSION := 34
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
